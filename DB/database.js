3d57f58f23250ebe4789db31a232074366abad24
Adrina bernadin <mary.adrina@kalvium.community>
2025-01-04 22:12:59 +0530

    changes

---
+++ DB/database.js
@@ -0,0 +1,13 @@
+if(process.env.NODE_ENV!=='PRODUCTION'){
+    require('dotenv').config()
+}
+const mongoose=require('mongoose')
+
+const connectDB=()=>{
+    mongoose
+        .connect(process.env.URL)
+        .then(() => console.log(`Database is connected successfully`))
+        .catch((err) => console.log('Database connection failed..', err.message))
+}
+
+module.exports=connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+if(process.env.NODE_ENV !=='PRODUCTION'){
+    require('dotenv').config()
+}
 const express=require('express');
+const connectDB= require("./DB/database.js")
 const app=express();
+const PORT=process.env.PORT || 3000
 
 app.get('/ping',(req,res)=> {
     return res.send('pong');
 })
 
-const PORT=3000
+
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`server is running on http://localhost:${PORT}`)
 })
